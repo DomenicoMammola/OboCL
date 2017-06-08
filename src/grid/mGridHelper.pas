d584a932e76a6bbd31dd938b5103a1476fb0fcfd
Domenico Mammola <mimmo71@gmail.com>
2017-06-08 16:01:11 +0200

    * sort values command by clicking on the title of a column in dbgrids

--- src/grid/mGridHelper.pas
+++ src/grid/mGridHelper.pas
@@ -19,7 +19,8 @@ interface
 uses
   Classes,
   DBGrids, Controls,
-  mGridColumnSettings, mXML, mGridSettingsForm;
+  mGridColumnSettings, mXML, mGridSettingsForm, mSortConditions, mGridIcons,
+  mDatasetInterfaces;
 
 type
 
@@ -29,27 +30,82 @@ type
   strict private
     FSettings : TmGridColumnsSettings;
     FDBGrid : TDBGrid;
+    FSortManager : ISortableDatasetManager;
+    FGridIcons: TmGridIconsDataModule;
+    procedure OnTitleClick (Column: TColumn);
   public
     constructor Create;
     destructor Destroy; override;
     function EditSettings : boolean;
     procedure LoadSettings (aStream : TStream);
     procedure SaveSettings (aStream : TStream);
+    procedure EnableSort (aSortManager : ISortableDatasetManager);
 
     property Grid : TDBGrid read FDBGrid write FDBGrid;
   end;
 
 implementation
 
+uses
+  SysUtils;
+
 { TmDBGridHelper }
+
+
+// inspired by http://forum.lazarus.freepascal.org/index.php?topic=24510.0
+procedure TmDBGridHelper.OnTitleClick(Column: TColumn);
+var
+  tmpSortType : TSortType;
+  tmpSortConditions : TSortByConditions;
+  i, idx : integer;
+begin
+  tmpSortType := stAscending;
+
+  // remove every arrow from column captions
+  for i := 0 to FDBGrid.Columns.Count - 1 do
+    FDBGrid.Columns[i].Title.ImageIndex := -1;
+
+  // analize current filter
+  if (FSortManager.GetSorted) and (FSortManager.GetSortByConditions.Count > 0) and (FSortManager.GetSortByConditions.Items[0].FieldName = Column.FieldName) then
+  begin
+    if FSortManager.GetSortByConditions.Items[0].SortType = stAscending then
+      tmpSortType:= stDescending
+    else
+    begin
+      FSortManager.ClearSort;
+      exit;
+    end
+  end;
+
+  // set new sort condition
+  FSortManager.GetSortByConditions.Clear;
+  with FSortManager.GetSortByConditions.Add do
+  begin
+    FieldName:= Column.FieldName;
+    SortType:= tmpSortType;
+  end;
+
+  // do sort
+  if FSortManager.Sort then
+  begin
+    if tmpSortType = stAscending then
+      idx := 0
+    else
+      idx := 1;
+    FDBGrid.Columns[Column.Index].Title.ImageIndex := idx;
+  end;
+end;
 
 constructor TmDBGridHelper.Create;
 begin
   FSettings := TmGridColumnsSettings.Create;
+  FSortManager := nil;
 end;
 
 destructor TmDBGridHelper.Destroy;
 begin
+  FSortManager := nil;
+  FDBGrid := nil;
   FSettings.Free;
   inherited Destroy;
 end;
@@ -107,6 +163,17 @@ begin
     doc.SaveToStream(aStream);
   finally
     doc.Free;
+  end;
+end;
+
+procedure TmDBGridHelper.EnableSort(aSortManager : ISortableDatasetManager);
+begin
+  if Assigned(FDBGrid) then
+  begin
+    FGridIcons:= TmGridIconsDataModule.Create(FDBGrid);
+    FDBGrid.TitleImageList := FGridIcons.GridImageList;
+    FDBGrid.OnTitleClick:= Self.OnTitleClick;
+    FSortManager:= aSortManager;
   end;
 end;
 

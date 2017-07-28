a1ce7949e5b5d8acba30ecc95cc4ad661447a546
Domenico Mammola <mimmo71@gmail.com>
2017-07-28 11:11:17 +0200

    * TDBGrid with custom checkbox bitmaps

---
+++ src/grid/images/create_lrs_file.bat
@@ -0,0 +1,1 @@
+c:\lazarus\tools\lazres ..\lcl_dbgrid_customimages.lrs dbgridcustomgrayedcb.bmp dbgridcustomcheckedcb.bmp dbgridcustomuncheckedcb.bmp
--- src/grid/mDBGrid.pas
+++ src/grid/mDBGrid.pas
@@ -1,20 +1,65 @@
 unit mDBGrid;
 
-{$IFDEF FPC}
-  {$MODE DELPHI}
-{$ENDIF}
-
 interface
 
 uses
-  DBGrids;
+  Classes, DBGrids, StdCtrls, Graphics;
 
 type
+
+  { TmDBGrid }
+
   TmDBGrid = class(TDBGrid)
+  strict private
+    FCustomUncheckedBitmap : TBitmap;
+    FCustomCheckedBitmap : TBitmap;
+    FCustomGrayedBitmap : TBitmap;
+  protected
+    function  GetImageForCheckBox(const aCol,aRow: Integer; CheckBoxView: TCheckBoxState): TBitmap; override;
   public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+
     property OnSelection;
   end;
 
 implementation
 
+uses
+  LResources;
+
+{ TmDBGrid }
+
+function TmDBGrid.GetImageForCheckBox(const aCol, aRow: Integer; CheckBoxView: TCheckBoxState): TBitmap;
+begin
+  if CheckboxView=cbUnchecked then
+    Result := FCustomUncheckedBitmap
+  else if CheckboxView=cbChecked then
+    Result := FCustomCheckedBitmap
+  else
+    Result := FCustomGrayedBitmap;
+end;
+
+constructor TmDBGrid.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  FCustomUnCheckedBitmap := TBitmap.Create;
+  FCustomUnCheckedBitmap.LoadFromLazarusResource('dbgridcustomuncheckedcb');
+  FCustomCheckedBitmap := TBitmap.Create;
+  FCustomCheckedBitmap.LoadFromLazarusResource('dbgridcustomcheckedcb');
+  FCustomGrayedBitmap := TBitmap.Create;
+  FCustomGrayedBitmap.LoadFromLazarusResource('dbgridcustomgrayedcb');
+end;
+
+destructor TmDBGrid.Destroy;
+begin
+  FCustomUncheckedBitmap.Free;
+  FCustomCheckedBitmap.Free;
+  FCustomGrayedBitmap.Free;
+  inherited Destroy;
+end;
+
+
+initialization
+  {$I lcl_dbgrid_customimages.lrs}
 end.

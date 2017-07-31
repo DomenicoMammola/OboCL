90d96e9eb1b289fba951d043b0d3f00957ca0fd3
Domenico Mammola <mimmo71@gmail.com>
2017-07-31 18:28:56 +0200

    * save mode was missing

--- src/dialogs/mfileopensavelcl.pas
+++ src/dialogs/mfileopensavelcl.pas
@@ -20,19 +20,27 @@ type
     ButtonPanel: TButtonPanel;
     EditFileName: TEdit;
     LVFiles: TListView;
+    PanelCurrentFolder: TPanel;
     Splitter1: TSplitter;
     TVFolders: TTreeView;
+    procedure EditFileNameEditingDone(Sender: TObject);
     procedure LVFilesDblClick(Sender: TObject);
     procedure LVFilesSelectItem(Sender: TObject; Item: TListItem;
       Selected: Boolean);
+    procedure OKButtonClick(Sender: TObject);
     procedure TVFoldersSelectionChanged(Sender: TObject);
   private
     FCurrentFolder : TmFolder;
     FCurrentFile : TmFile;
+    FSaveFileData : TmFileData;
     FFileOperation : TmFileOperation;
+    FManager : TmAbstractFileSystemManager;
     procedure AddNode (aParent : TTreeNode; aFolder : TmFolder);
     procedure RefreshFiles;
   public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+
     procedure Init (aFileSystemManager : TmAbstractFileSystemManager; const aFileOperation : TmFileOperation);
     function GetFileData: TmFileData;
   end;
@@ -47,9 +55,15 @@ implementation
 procedure TmFileOpenSave.TVFoldersSelectionChanged(Sender: TObject);
 begin
   if TVFolders.SelectionCount > 0 then
-    FCurrentFolder := TmFolder(TVFolders.Selected.Data)
+  begin
+    FCurrentFolder := TmFolder(TVFolders.Selected.Data);
+    PanelCurrentFolder.Caption:= FCurrentFolder.Path;
+  end
   else
+  begin
     FCurrentFolder := nil;
+    PanelCurrentFolder.Caption:= '';
+  end;
   RefreshFiles;
 end;
 
@@ -58,8 +72,22 @@ begin
   EditFileName.Text := '';
   if Assigned(Item) and Assigned (FCurrentFolder) then
   begin
-    EditFileName.Text := TmFile(Item.Data).FileData.GetFullPath;
+    EditFileName.Text := TmFile(Item.Data).FileData.FileName;
     FCurrentFile := TmFile(Item.Data);
+  end;
+end;
+
+procedure TmFileOpenSave.OKButtonClick(Sender: TObject);
+begin
+  if FFileOperation = fmOpen then
+  begin
+    if not Assigned(FCurrentFile) then
+      ModalResult:= mrCancel;
+  end
+  else if FFileOperation = fmSave then
+  begin
+    if not Assigned(FCurrentFolder) or (trim(EditFileName.Text) = '') then
+      ModalResult := mrCancel;
   end;
 end;
 
@@ -70,6 +98,11 @@ begin
     FCurrentFile := TmFile(LVFiles.Selected.Data);
     Self.ModalResult:= mrOk;
   end;
+end;
+
+procedure TmFileOpenSave.EditFileNameEditingDone(Sender: TObject);
+begin
+  EditFileName.Text:= FManager.ValidateFileName(EditFileName.Text);
 end;
 
 procedure TmFileOpenSave.AddNode(aParent: TTreeNode; aFolder: TmFolder);
@@ -104,11 +137,25 @@ begin
   end;
 end;
 
+constructor TmFileOpenSave.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  FSaveFileData := TmFileData.Create;
+end;
+
+destructor TmFileOpenSave.Destroy;
+begin
+  FSaveFileData.Free;
+  inherited Destroy;
+end;
+
 procedure TmFileOpenSave.Init(aFileSystemManager: TmAbstractFileSystemManager; const aFileOperation : TmFileOperation);
 var
   i : integer;
 begin
   FFileOperation:= aFileOperation;
+  FManager := aFileSystemManager;
+
   TVFolders.BeginUpdate;
   try
     TVFolders.Items.Clear;
@@ -132,6 +179,12 @@ begin
     begin
       Result := FCurrentFile.FileData;
     end;
+  end
+  else if FFileOperation = fmSave then
+  begin
+    FSaveFileData.FileName:= EditFileName.Text;
+    FSaveFileData.Path:= FCurrentFolder.Path;
+    Result := FSaveFileData;
   end;
 end;
 

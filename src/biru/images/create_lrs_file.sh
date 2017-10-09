d51888fac06b1f8bbe76d28c8bbcc77d1bbd329b
Domenico Mammola <mimmo71@gmail.com>
2017-10-09 12:33:21 +0200

    * updated scripts

---
+++ src/biru/images/create_lrs_file.sh
@@ -0,0 +1,1 @@
+lazres ../lcl_biru_freshfruit_images.lrs fixedbackground.bmp scrollingbackground_sea.bmp banana.bmp banana_mask.bmp kiwi.bmp kiwi_mask.bmp cherry.bmp cherry_mask.bmp apple.bmp apple_mask.bmp coconut.bmp coconut_mask.bmp grapefruit.bmp grapefruit_mask.bmp lemon.bmp lemon_mask.bmp strawberry.bmp strawberry_mask.bmp watermelon.bmp watermelon_mask.bmp

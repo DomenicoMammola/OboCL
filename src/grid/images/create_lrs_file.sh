20e5e6a72363d1ebc71d5f38076b6ce1b24b99c5
DomenicoMammola <mimmo71@gmail.com>
2020-03-19 12:49:30 +0100

    * lower case for resource file

--- src/grid/images/create_lrs_file.sh
+++ src/grid/images/create_lrs_file.sh
@@ -1,1 +1,1 @@
-/usr/share/lazarus/1.8.2/tools/lazres ../lcl_dbgrid_customimages.lrs dbgridcustomgrayedcb.bmp dbgridcustomcheckedcb.bmp dbgridcustomuncheckedcb.bmp
+/usr/share/lazarus/2.0.2/tools/lazres ../lcl_dbgrid_customimages.lrs dbgridcustomgrayedcb.bmp dbgridcustomcheckedcb.bmp dbgridcustomuncheckedcb.bmp

9fa613f947c0f8d3feb680f8cf59985ae9ae3a22
Domenico Mammola <mimmo71@gmail.com>
2022-03-29 17:29:32 +0200

    * in the "advanced" tab now it is possibile to filter key values with "starts with" or "ends with" conditions (in addiction to the existing "contains" filtering operation)

--- src/grid/mgridfiltervaluesdlg.pas
+++ src/grid/mgridfiltervaluesdlg.pas
@@ -15,15 +15,17 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ButtonPanel,
-  CheckLst, ExtCtrls, ComCtrls, StdCtrls, Menus, ListFilterEdit;
+  CheckLst, ExtCtrls, ComCtrls, StdCtrls, Menus, ListFilterEdit, EditBtn;
 
 resourcestring
   SExportValuesMenuCaption = 'Export values...';
   SWarningFileExists = 'File exists';
   SAskOverwriteFile = 'File already exists. Overwrite it?';
+  SFilterModeContains = 'Contains';
+  SFilterModeStartsWith = 'Starts with';
+  SFilterModeEndsWith = 'Ends with';
 
 type
-
   { TFilterValuesDlg }
 
   TFilterValuesDlg = class(TForm)
@@ -33,11 +35,15 @@ type
     ListBoxToBeFiltered: TListBox;
     ListBoxFilter: TListBox;
     ListFilterEditAdvanced: TListFilterEdit;
+    MI_AdvancesSearhEndsWith: TMenuItem;
+    MI_AdvancedSearchStartsWith: TMenuItem;
+    MI_AdvancedSearchContains: TMenuItem;
     MI_ExportValues: TMenuItem;
     PageControlFilter: TPageControl;
     PanelTop: TPanel;
     PanelRight: TPanel;
     PanelLeft: TPanel;
+    PopupMenuAdvancedSearch: TPopupMenu;
     PopupMenuValues: TPopupMenu;
     Splitter1: TSplitter;
     TSAdvanced: TTabSheet;
@@ -50,10 +56,14 @@ type
     procedure FormShow(Sender: TObject);
     procedure ListBoxFilterClick(Sender: TObject);
     procedure ListBoxToBeFilteredClick(Sender: TObject);
+    function ListFilterEditAdvancedFilterItemEx(const ACaption: string; ItemData: Pointer; out Done: Boolean): Boolean;
+    procedure MI_AdvancedSearchContainsClick(Sender: TObject);
+    procedure MI_AdvancedSearchStartsWithClick(Sender: TObject);
+    procedure MI_AdvancesSearhEndsWithClick(Sender: TObject);
     procedure MI_ExportValuesClick(Sender: TObject);
   private
-    { private declarations }
   public
+    constructor CreateNew(AOwner: TComponent; Num: Integer = 0); override;
     procedure Init (const aList : TStringList);
     procedure GetCheckedValues (aList : TStringList);
   end;
@@ -61,7 +71,8 @@ type
 implementation
 
 uses
-  mWaitCursor;
+  StrUtils,
+  mWaitCursor, mFormSetup;
 
 {$R *.lfm}
 
@@ -75,10 +86,14 @@ end;
 procedure TFilterValuesDlg.FormCreate(Sender: TObject);
 begin
   MI_ExportValues.Caption:= SExportValuesMenuCaption;
+  MI_AdvancedSearchContains.Caption:= SFilterModeContains;
+  MI_AdvancedSearchStartsWith.Caption:= SFilterModeStartsWith;
+  MI_AdvancesSearhEndsWith.Caption:= SFilterModeEndsWith;
 end;
 
 procedure TFilterValuesDlg.FormShow(Sender: TObject);
 begin
+  PageControlFilter.ActivePageIndex:= 0;
   ListFilterEdit.SetFocus;
 end;
 
@@ -97,6 +112,66 @@ begin
     tmp := ListBoxToBeFiltered.Items[ListBoxToBeFiltered.ItemIndex];
     if ListBoxFilter.Items.IndexOf(tmp) < 0 then
       ListBoxFilter.Items.Append(tmp);
+  end;
+end;
+
+function TFilterValuesDlg.ListFilterEditAdvancedFilterItemEx(const ACaption: string; ItemData: Pointer; out Done: Boolean): Boolean;
+begin
+  Result := false;
+  Done := false;
+  if MI_AdvancedSearchContains.Checked then
+    exit
+  else if MI_AdvancedSearchStartsWith.Checked then
+  begin
+    if fsoCaseSensitive in ListFilterEditAdvanced.FilterOptions then
+      Result := StartsStr(ListFilterEditAdvanced.Filter, ACaption)
+    else
+      Result := StartsText(ListFilterEditAdvanced.Filter, ACaption);
+    Done := true;
+  end
+  else if MI_AdvancesSearhEndsWith.Checked then
+  begin
+    if fsoCaseSensitive in ListFilterEditAdvanced.FilterOptions then
+      Result := EndsStr(ListFilterEditAdvanced.Filter, ACaption)
+    else
+      Result := EndsText(ListFilterEditAdvanced.Filter, ACaption);
+    Done := true;
+  end;
+end;
+
+procedure TFilterValuesDlg.MI_AdvancedSearchContainsClick(Sender: TObject);
+begin
+  if not MI_AdvancedSearchContains.Checked then
+  begin
+    MI_AdvancedSearchContains.Checked := true;
+    MI_AdvancedSearchStartsWith.Checked:= false;
+    MI_AdvancesSearhEndsWith.Checked:= false;
+    ListFilterEditAdvanced.InvalidateFilter;
+    ListFilterEditAdvanced.ForceFilter(ListFilterEditAdvanced.Filter);
+  end;
+end;
+
+procedure TFilterValuesDlg.MI_AdvancedSearchStartsWithClick(Sender: TObject);
+begin
+  if not MI_AdvancedSearchStartsWith.Checked then
+  begin
+    MI_AdvancedSearchContains.Checked := false;
+    MI_AdvancedSearchStartsWith.Checked:= true;
+    MI_AdvancesSearhEndsWith.Checked:= false;
+    ListFilterEditAdvanced.InvalidateFilter;
+    ListFilterEditAdvanced.ForceFilter(ListFilterEditAdvanced.Filter);
+  end;
+end;
+
+procedure TFilterValuesDlg.MI_AdvancesSearhEndsWithClick(Sender: TObject);
+begin
+  if not MI_AdvancesSearhEndsWith.Checked then
+  begin
+    MI_AdvancedSearchContains.Checked := false;
+    MI_AdvancedSearchStartsWith.Checked:= false;
+    MI_AdvancesSearhEndsWith.Checked:= true;
+    ListFilterEditAdvanced.InvalidateFilter;
+    ListFilterEditAdvanced.ForceFilter(ListFilterEditAdvanced.Filter);
   end;
 end;
 
@@ -134,6 +209,12 @@ begin
   finally
     dlg.Free;
   end;
+end;
+
+constructor TFilterValuesDlg.CreateNew(AOwner: TComponent; Num: Integer);
+begin
+  inherited CreateNew(AOwner, Num);
+  SetupFormAndCenter(Self);
 end;
 
 procedure TFilterValuesDlg.BtnAddAllClick(Sender: TObject);

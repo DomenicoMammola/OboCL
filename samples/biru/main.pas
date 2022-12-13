15cc55a3cdbbff16967d95d53d7e84d7ee04b323
Domenico Mammola <mimmo71@gmail.com>
2022-12-13 17:12:36 +0100

    * update biru sample

--- samples/biru/main.pas
+++ samples/biru/main.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  ExtCtrls, Spin, Biru, Biru_FreshFruit;
+  ExtCtrls, Spin, Biru;
 
 type
 
@@ -15,14 +15,15 @@ type
   TForm1 = class(TForm)
     Button1: TButton;
     Button2: TButton;
-    Label1: TLabel;
+    PanelBottom: TPanel;
     RadioGroup1: TRadioGroup;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
     { private declarations }
-    FBiru : TBiruFreshFruit;
+    FBiru : TBiru;
   public
     { public declarations }
   end;
@@ -37,13 +38,36 @@ implementation
 { TForm1 }
 
 procedure TForm1.FormCreate(Sender: TObject);
+var
+  tmpBitmap, tmpBitmapMask : TBitmap;
 begin
-  FBiru := TBiruFreshFruit.Create(Self);
+  FBiru := TBiru.Create(Self);
   FBiru.Parent := Self;
   FBiru.Top := 0;
   FBiru.Left := 0;
-  FBiru.Shape := bsBanana;
+
+  tmpBitmap := TBitmap.Create;
+  tmpBitmap.LoadFromFile('apple.bmp');
+  tmpBitmapMask := TBitmap.Create;
+  tmpBitmapMask.LoadFromFile('apple_mask.bmp');
+  FBiru.AddBiruImageAndMask(tmpBitmap, tmpBitmapMask);
+
+  tmpBitmap := TBitmap.Create;
+  tmpBitmap.LoadFromFile('strawberry.bmp');
+  tmpBitmapMask := TBitmap.Create;
+  tmpBitmapMask.LoadFromFile('strawberry_mask.bmp');
+  FBiru.AddBiruImageAndMask(tmpBitmap, tmpBitmapMask);
+
+  FBiru.FixedBackground.LoadFromFile('longbackground.bmp');
+  FBiru.ScrollingBackground.LoadFromFile('longbackground.bmp'); //'scrollingbackground_vegetables.bmp');
+
+  FBiru.ImageIndex := 0;
   FBiru.Animation:= tatBouncing;
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  //
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
